5f133775e8eacb534f23d1f2699024b160bae790
MihailCoscodan <mihail.coscodan@acconomy.com>
2022-02-28 18:37:09 +0200

    First commit

---
+++ SafeQueueExample/SafeQueueExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SafeQueueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
